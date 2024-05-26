790030e588a9f4a0883b4a6517a706db21a64837
George-Harpum <gesharpum@gmail.com>
2024-05-26 21:42:42 +0100

    added word_count func

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ Program that provides some basic information around the book
 """
 def main(file):
     full_file = read_contents(file)
-    return full_file
+    count = word_count(full_file)
+    return count
 
 def read_contents(file: str) -> str:
     with open(file) as f:
         contents = f.readlines()
     return " ".join(contents)
 
+def word_count(s: str):
+    return len(s.split())
+
 if __name__ == "__main__":
     FILE = "./books/frankenstein.txt"
     print(main(FILE))
+
